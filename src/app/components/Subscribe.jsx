4004183f3c8f1dcdbbc395f65a147897850a73e0
hassan <hassanbajwa154@gmail.com>
2024-07-27 00:12:17 +0500

    update

--- src/app/components/Subscribe.jsx
+++ src/app/components/Subscribe.jsx
@@ -45,7 +45,7 @@ function Subscribe() {
         <div className="flex items-center justify-between border -mt-3 bg-white px-4 py-[13px] rounded-full">
           <input
             placeholder="Card Number"
-            className="outline-none max-w-[160px] sm:w-auto text-[15px]"
+            className="outline-none max-w-[180px] sm:w-auto text-[15px]"
           />
           <div>
             <div class="flex items-center">
